8c3630b9412f82213d125336370f5f57a041598c
Thompson Plyler <nealok.g@gmail.com>
2019-12-03 10:50:18 -0500

    Countdown Added. Teaching for 7yo added.

--- moment-overlay/main.js
+++ moment-overlay/main.js
@@ -1,18 +1,28 @@
-const time = document.getElementById("moment")
+const getDuration = () => {
+    const time = document.getElementById("moment")
+    
 
-const getTimeRemaining = () => {
-    let now = moment();
-    let deadline = now.clone().hour(20).minute(40).second(0);
-    console.log(deadline)
-    if (now.isAfter(deadline)) {
-        // disable RSVP button here
-        return "Closed";
-    } else {
-        // enable RSVP button here
-        // returns “in x hours”, “in x minutes”, “in a few seconds”
-        console.log(deadline.from(now))
-        return deadline.from(now, "HH-mm-ss");
+    const now = moment().unix()
+    const deadline = moment().clone().hour(18).minute(00).second(0).unix();
+
+
+    const difference = deadline-now;
+    const hours = Math.floor(difference / 3600);
+    const minutes = Math.floor((difference % 3600) / 60);
+    const seconds = Math.floor( difference % 60) 
+
+    if (hours > 1){
+        time.innerHTML = `${hours} hours, ${minutes} minutes, ${seconds} seconds`    
+    }
+    else if (hours < 1 && minutes > 1){
+        time.innerHTML = `${minutes} minutes, ${seconds} seconds`
+    }
+    else if (seconds < 0) {
+        time.innerHTML = "SEMPER FI!"
+    }
+    else {
+        time.innerHTML = `${seconds} seconds.`
     }
 }
 
-time.innerHTML = getTimeRemaining()
+const timer = setInterval(getDuration,1000)
