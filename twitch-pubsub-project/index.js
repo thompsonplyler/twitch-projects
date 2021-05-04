87eb088164031d74be8a363fdf83f1a46621c29b
Thompson Plyler <nealok.g@gmail.com>
2021-05-03 20:20:00 -0400

    Removed unnecessary files. Added ENV file. Lots of changes to read info from chat and send to websocket.

--- twitch-pubsub-project/index.js
+++ twitch-pubsub-project/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const tmi = require('tmi.js')
 const fetch = require('node-fetch')
 const moment = require('moment');
@@ -5,6 +6,9 @@ const moment = require('moment');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const io = require('socket.io-client')
+
+
 const port = 3002;
 // required to connect to Twitch
 const options = {
@@ -15,10 +19,12 @@ const options = {
     },
     identity: {
         username: 'PubSubBot',
-        password: 'oauth:c1kv42il865ms9d8ooijyrv3ikzs73'
+        password: process.env.PUBSUBPASSWORD
     },
     channels: ['gamemasterthompson']
 }
+
+let goldState = true
 
 app.use(cors())
 
@@ -34,25 +40,48 @@ client.on('connected', (address, port) => {
     // interpret incoming messages
     .on('message', (channel, userstate, message, self) => {
 
-        console.log("This is the message:", JSON.stringify(userstate))
+        console.log("This is the message:", `
+        
+        
+        ${JSON.stringify(userstate["custom-reward-id"])}
+        
+        
+        
+        `)
+        let rewardNum = "09083718-497b-4c63-ac42-0d17f8f584e8"
+
+        if(userstate["custom-reward-id"]==rewardNum){
+            console.log(`Ya made it: ${rewardNum}`)
+            if (goldState){
+                goldSpend()
+                }
+            }
+
         objMessage.messageBody = message
         objMessage.userState = userstate
+
+
 
         // look for command
         if (message.charAt(0) === "!") {
             // look for timer command
-            if (message.split(' ')[0] === "!timer") {
-                const firstArgument = message.split(' ')[1]
-                const secondArgument = message.split(' ')[2]
-                console.log(`!timer command executed: Command: ${message.split(' ')[0]} Region: ${firstArgument} SummonerName: ${secondArgument}`)
-                timerCommand(firstArgument, secondArgument, message, channel);
-            }
+            
             if (message.split(' ')[0] === "!today") {
                 const firstArgument = message.split(' ')[1]
                 const secondArgument = message.split(' ')[2]
                 console.log(`!today command executed: Command: ${message.split(' ')[0]} Region: ${firstArgument} SummonerName: ${secondArgument}`)
                 todayCommand(firstArgument, secondArgument, message, channel);
             }
+            
+            // if (message.split(' ')[0]=== "!spendyourgold"){
+            //     if (goldState){
+            //     goldSpend()
+            //     }
+            //     else {
+            //         client.say(channel, "Nice try, guy. That command was JUST used.")
+            //     }
+
+            // }
         }
         if (self) return;
         if (message.toLowerCase() === '!hello') {
@@ -91,28 +120,6 @@ client.on('connected', (address, port) => {
         `)
     })
 
-const timerCommand = (region, argument, receivedMessage, channel) => {
-    argument = encodeURIComponent(argument);
-
-    if (argument.length > 0) {
-        fetch(`http://localhost:3001/api/v1/timer/?summoner_name=${argument}&region=${region}`, {
-            method: 'POST'
-        })
-            .then(r => r.json())
-            // .then(r=> console.log(r))
-            .then(response => {
-                argument = decodeURIComponent(argument);
-                responseHandler(response, receivedMessage, argument, channel);
-
-            }
-            )
-            .catch(error => console.log('This is the error you received: ' + error));
-
-    } else {
-        client.say(channel, `I've received the timer command, but your parameters made no sense. Syntax is: "!timer <region> <summonername>`);
-    }
-};
-
 const todayCommand = (region, argument, receivedMessage, channel) => {
     argument = encodeURIComponent(argument);
     console.log(`
@@ -148,45 +155,12 @@ const todayCommand = (region, argument, receivedMessage, channel) => {
     }
 };
 
-const responseHandler = (data, receivedMessage, argument, channel) => {
-    if (data.grand_total_time === '00 hours 00 minutes and 00 seconds') {
-        client.say(channel, `**Weekly Results for Summoner:** ${argument}\nThis summoner hasn't played any games this week.`);
-    } else {
-        client.say(channel,
-            `**Weekly Results for Summoner:** ${argument}\n${handleToday(data)}\n${handleYesterday(data)}\n${handleMinusTwo(data)}\n${handleMinusThree(data)}\n${handleMinusFour(data)}\n${handleMinusFive(data)}\n${handleMinusSix(data)}
-      \n**Time Played This Week:** ${data.grand_total_time}\n**Win Percentage:** ${winCount(data.grand_total_results)}%`
-        );
-    }
-};
+
 
 const todayResponseHandler = (data, receivedMessage, argument, channel) => {
-
-    // results = {
-    //     won:wonGames,
-    //     lost:failGames, 
-    //     total:totalGames,
-    //     percent:percentage}
     let results = winCount(data)
     client.say(channel,`${argument}'s record today is ${results.won} - ${results.lost}, a ${results.percent}% win rate`)
     return
-    if (data.grand_total_time == '00 hours 00 minutes and 00 seconds') {
-        client.say(channel, `Today's Results for ${argument}: This summoner hasn't played any games today.`);
-    } else {
-        if (argument.toLowerCase() == "deusrektmachina") {
-            argument = "gamema24Gmtoorah1"
-        }
-        client.say(channel,
-            `${argument} has played for ${data.grand_total_time} today and won ${winCount(data.grand_total_results)}% of ${data.grand_total_results.length} games played.`
-        );
-    }
-};
-
-const handleToday = (data) => {
-    if (data.today.total_time === '00 hours 00 minutes and 00 seconds') {
-        return '**Today:** No games played.';
-    } else {
-        return `**Today:** Played for ${data.today.total_time} -- Won ${winCount(data.today.results)}% of total games played.`;
-    }
 };
 
 const winCount = (data) => {
@@ -209,43 +183,7 @@ const winCount = (data) => {
         total:totalGames,
         percent:percentage}
 
-    // if (percentage) {
-    //     return Math.floor(percentage * 100);
-    // } return '0';
     return results
-};
-
-const momentCommand = (value) => {
-    const now = moment();
-    console.log(now)
-    let day = now.add(value, 'days').day();
-    switch (day) {
-        case 0:
-            day = 'Sunday';
-            break;
-        case 1:
-            day = 'Monday';
-            break;
-        case 2:
-            day = 'Tuesday';
-            break;
-        case 3:
-            day = 'Wednesday';
-            break;
-        case 4:
-            day = 'Thursday';
-            break;
-        case 5:
-            day = 'Friday';
-            break;
-        case 6:
-            day = 'Saturday';
-            break;
-        default:
-            day = 'Sunday';
-    }
-    console.log(day)
-    return day;
 };
 
 app.get("/subs", (req, res) => {
@@ -254,3 +192,24 @@ app.get("/subs", (req, res) => {
 })
 
 app.listen(port, () => console.log(`Subscriber monitor is listening on port ${port}!`))
+
+// connects PubSub server to local socket.io server
+const socket = io('http://localhost:3000');
+
+// function to respond to !spendyourgold
+const goldSpend = () => {
+    socket.emit('gold')
+    // resetGold()
+}
+
+const resetGold = () => {
+    goldState = false
+
+    const resetGoldState = () => {
+        goldState = true
+        return
+    }
+
+    setTimeout(resetGoldState,15000)
+    return 
+}
--- twitch-pubsub-project/twitchClient.js
+++
@@ -1,5 +0,0 @@
-import TwitchClient from 'twitch';
-
-const clientId = '123abc';
-const accessToken = 'def456';
-const twitchClient = await TwitchClient.withCredentials(clientId, accessToken);
