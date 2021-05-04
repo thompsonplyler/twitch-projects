112679f5fc1c98699c3de0228c3fd2f97a6b8223
thompsonplyler <nealok.g@gmail.com>
2021-05-03 20:24:15 -0400

    Delete react-twitch-project directory

--- react-twitch-project/src/components/Gamestream.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function GameStream() {
-    return (
-        <div>Game Stream Component</div>
-    )
-}
-
-export default GameStream
--- react-twitch-project/src/components/Stream.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Streams() {
-    return (
-        <div>Streams Component</div>
-    )
-}
-
-export default Streams
